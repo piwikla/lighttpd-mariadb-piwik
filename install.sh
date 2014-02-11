b59264fc901c2142067820eafc56faa89e788d23
xiaoq-in <54jack@gmail.com>
2014-02-11 21:11:24 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ sudo apt-get install lighttpd php5-cgi
 #Install MariaDB
 sudo apt-get install software-properties-common
 sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
-sudo add-apt-repository 'deb http://ftp.yz.yamagata-u.ac.jp/pub/dbms/mariadb/repo/10.0/ubuntu saucy main'
+sudo add-apt-repository 'deb http://mirror.jmu.edu/pub/mariadb/repo/10.0/ubuntu saucy main'
 sudo apt-get update
 sudo apt-get install mariadb-server
 
